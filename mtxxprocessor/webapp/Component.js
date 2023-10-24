114b1002fff5af7b6373c5b591c42d531c4c329b
soyuztechnologies <contact@soyuztechnologies.com>
2023-10-24 13:48:41 +0530

    my first stable RAP app

---
+++ mtxxprocessor/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("mt.xx.proc.mtxxprocessor.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
